c7e2d08604980c268331905580af0e59104d33ec
Fradeiro2003 <134124028+Fradeiro2003@users.noreply.github.com>
2023-05-27 00:25:59 -0600

    Add files via upload

---
+++ prueba_de_overriding.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BaseClass {
+    public:
+        void disp(){
+            cout<<"Function of Parent Class"<<endl;
+}
+};
+
+class DerivedClass: public BaseClass{
+    public:
+        void disp(){
+            cout<<"Function of Child Class"<<endl;
+}
+};
+
+int main(){
+    BaseClass obj;
+    obj.disp();
+
+    DerivedClass obj2;
+    obj2.disp();
+}
